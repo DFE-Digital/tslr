d554bfcff2018ad9297d4655926abbf08a40fd64
James <james.dalton@infinityworks.com>
2018-12-12 14:02:50 +0000

    undone sen changes

--- TSLR.UI/middleware/student-loan.js
+++ TSLR.UI/middleware/student-loan.js
@@ -1,33 +1,13 @@
-import axios from 'axios'
-export default async function({ store, route, redirect }) {
+export default function ({ store, route, redirect }) {
   if (route.path.includes('validator/student-loan')) {
     if (route.query.paid) {
       if (route.query.paid.includes('true')) {
-        let schoolRes = await axios
-          .get(`/api/Schools/${route.query.schoolId}/sen`)
-          .then(res => {
-            console.log('returned :' + res.data)
-            return res.data
-          })
-          .catch(err => {
-            console.log('Error: ' + err)
-            redirect(
-              `/question/student-loan?invalid=true&schoolId=${
-                route.query.schoolId
-              }`
-            )
-          })
-        if (schoolRes) {
-          redirect('/question/still-teaching-uk')
-        } else {
-          redirect(`/question/subjects-taught?school=${route.query.school}`)
-        }
+        redirect(`/question/subjects-taught?school=${route.query.school}`)
       } else if (route.query.paid.includes('false')) {
         redirect('/not-eligible/student-loan')
       }
     }
-    redirect(
-      `/question/student-loan?invalid=true&schoolId=${route.query.schoolId}`
-    )
+    redirect(`/question/student-loan?invalid=true&school=${route.query.school}`)
   }
 }
+
