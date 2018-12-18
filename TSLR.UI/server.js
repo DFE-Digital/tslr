95232996487317d04ed4660a32e5ee63c9676991
James <james.dalton@infinityworks.com>
2018-12-18 22:01:40 +0000

    Added self signed cert and http redirect

--- TSLR.UI/server.js
+++ TSLR.UI/server.js
@@ -1,44 +1,47 @@
 const { Nuxt, Builder } = require('nuxt')
-
+const fs = require('fs')
+const express = require('express')
+const app = express()
 const isProd = process.env.NODE_ENV === 'production'
-
+const http = require('http')
+const https = require('https')
 // We instantiate Nuxt.js with the options
 const config = require('./nuxt.config.js')
 config.dev = !isProd
 const nuxt = new Nuxt(config)
 
-new Builder(nuxt).build()
+// Render every route with Nuxt.js
+app.use(nuxt.render)
 
-require('greenlock-express')
-  .create({
-    // Let's Encrypt v2 is ACME draft 11
-    version: 'draft-11',
+// Build only in dev mode with hot-reloading
+if (config.dev) {
+  new Builder(nuxt)
+    .build()
+    .then(listen)
+    .catch(error => {
+      console.error(error)
+      process.exit(1)
+    })
+} else {
+  listen()
+}
 
-    // Note: If at first you don't succeed, switch to staging to debug
-    // https://acme-staging-v02.api.letsencrypt.org/directory
-    server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
+var options = {
+  key: fs.readFileSync('./ssl/privatekey.pem'),
+  cert: fs.readFileSync('./ssl/certificate.pem')
+}
 
-    // Where the certs will be saved, MUST have write access
-    configDir: './.config/acme/',
-
-    // You MUST change this to a valid email address
-    email: 'james.dalton@infinityworks.com',
-
-    // You MUST change these to valid domains
-    // NOTE: all domains will validated and listed on the certificate
-    approvedDomains: ['tslr-eligiblity-ci.westeurope.cloudapp.azure.com'],
-
-    // You MUST NOT build clients that accept the ToS without asking the user
-    agreeTos: true,
-
-    app: require('express')().use(nuxt.render),
-
-    // Join the community to get notified of important updates
-    communityMember: true,
-
-    // Contribute telemetry data to the project
-    telemetry: true
-
-    //, debug: true
-  })
-  .listen(80, 443)
+// Listen the server
+function listen() {
+  // Listen the server
+  http
+    .createServer(function(req, res) {
+      res.writeHead(301, {
+        Location: 'https://' + req.headers['host'] + req.url
+      })
+      res.end()
+    })
+    .listen(80)
+  https.createServer(options, app).listen(443)
+  console.log('Server listening on `localhost:')
+}
