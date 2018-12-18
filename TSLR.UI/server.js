785ab0ff41d9293a03a109322dee6f9bdf683632
James <james.dalton@infinityworks.com>
2018-12-18 19:03:17 +0000

    Using greenlock

--- TSLR.UI/server.js
+++ TSLR.UI/server.js
@@ -1,34 +1,42 @@
-const { Nuxt, Builder } = require('nuxt')
+const { Nuxt } = require('nuxt')
 
-const express = require('express')
-const app = express()
 const isProd = process.env.NODE_ENV === 'production'
-const port = process.env.PORT || 80
 
 // We instantiate Nuxt.js with the options
 const config = require('./nuxt.config.js')
 config.dev = !isProd
 const nuxt = new Nuxt(config)
 
-// Render every route with Nuxt.js
-app.use(nuxt.render)
+require('greenlock-express')
+  .create({
+    // Let's Encrypt v2 is ACME draft 11
+    version: 'draft-11',
 
-// Build only in dev mode with hot-reloading
-if (config.dev) {
-  new Builder(nuxt)
-    .build()
-    .then(listen)
-    .catch(error => {
-      console.error(error)
-      process.exit(1)
-    })
-} else {
-  listen()
-}
+    // Note: If at first you don't succeed, switch to staging to debug
+    // https://acme-staging-v02.api.letsencrypt.org/directory
+    server: 'https://acme-v02.api.letsencrypt.org/directory',
 
-// Listen the server
-function listen() {
-  // Listen the server
-  app.listen(port, '0.0.0.0')
-  console.log('Server listening on `localhost:' + port + '`.')
-}
+    // Where the certs will be saved, MUST have write access
+    configDir: '~/.config/acme/',
+
+    // You MUST change this to a valid email address
+    email: 'james.dalton@infinityworks.com',
+
+    // You MUST change these to valid domains
+    // NOTE: all domains will validated and listed on the certificate
+    approvedDomains: ['tslr-eligiblity-ci.westeurope.cloudapp.azure.com'],
+
+    // You MUST NOT build clients that accept the ToS without asking the user
+    agreeTos: true,
+
+    app: require('express')().use(nuxt.render),
+
+    // Join the community to get notified of important updates
+    communityMember: true,
+
+    // Contribute telemetry data to the project
+    telemetry: true
+
+    //, debug: true
+  })
+  .listen(80, 443)
