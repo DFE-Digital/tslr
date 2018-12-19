985ad57eaf3fabaf026f57e04fd892da203f0025
James <james.dalton@infinityworks.com>
2018-12-19 09:49:11 +0000

    Changed the start page to be the index

--- TSLR.UI/pages/index.vue
+++ TSLR.UI/pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <section class="container">
-    <div>
-      <nuxt-link to="/eligible-start">Start</nuxt-link>
+  <div class="govuk-grid-row">
+    <div class="govuk-grid-column-two-thirds">
+      <div class="govuk-heading-xl">
+        Teachers: check if you can claim back student loan payments
+      </div>
+      <p class="govuk-body">
+        You'll be asked questions to check whether you're eligible to claim your student loan payments back.
+      </p>
+      <p class="govuk-body">
+        Applications to claim are open from September 2019.
+      </p>
+      <p class="govuk-body">
+        If you've worked at more than one school since 6 April 2018, check your eligiblity separately for each school.
+      </p>
+      <nuxt-link to="/question/qualified" role="button" draggable="false" class="govuk-button" tabindex="4">
+        Continue to questions
+      </nuxt-link>
     </div>
-  </section>
+  </div>
 </template>
+
+<script>
+export default {
+  head: {
+    title: 'Eligibility checker'
+  }
+}
+</script>
